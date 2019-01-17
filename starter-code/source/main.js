b6f8bbc18f63ef1753095f9e58a26bd668004095
Julia Miller <ihre.hexenhafte.hoheit@gmail.com>
2019-01-17 16:01:01 +0100

    add comments and clean up functions

--- starter-code/source/main.js
+++ starter-code/source/main.js
@@ -3,26 +3,27 @@ var ctx = canvas.getContext('2d')
 var width = canvas.width
 var height = canvas.height
 
-// Iteration 1
+var player = new Character(0,0) // (0,0) = Initial position
+var winningScore = 10;
+var imgPlayer = new Image();
+
+// Draw the grid
 function drawGrid() {
   for(var i = 1; i < 10; i++){
     ctx.beginPath();
     ctx.moveTo(0, 50*i);
-    ctx.lineTo(500,50*i);
+    ctx.lineTo(width,50*i);
     ctx.closePath();
     ctx.stroke();
     ctx.beginPath();
     ctx.moveTo(50*i, 0);
-    ctx.lineTo(50*i,500);
+    ctx.lineTo(50*i, height);
     ctx.closePath();
     ctx.stroke();
   }
 }
-// Iteration 2
-var player = new Character(0,0) // (0,0) = Initial position
 
-// Iteration 3
-var imgPlayer = new Image();
+//Draw player image
 imgPlayer.src = "images/character-down.png";
 function drawPlayer() {
   imgPlayer.onload = function(){
@@ -30,10 +31,9 @@ function drawPlayer() {
   }
 }
 
-// Iteration 4
+// Draw treasure image
 var treasure = new Treasure;
 treasure.setRandomPosition();
-
 function drawTreasure() {
   var imgTreasure = new Image();
   imgTreasure.src = "images/treasure.png"
@@ -42,15 +42,37 @@ function drawTreasure() {
   }
 }
 
+//check if player found the treasure
 function checkIfFoundTreasure(){
-  if(player.col === treasure.col && player.row === treasure.row) return true;
-  else return false;
+  if(player.col === treasure.col && player.row === treasure.row) {
+    treasure.setRandomPosition();
+    player.points ++;
+    if(player.points === winningScore) {
+      alert("Congratulations! You win!");
+      player.row = 0; 
+      player.col = 0;
+      player.points = 0;
+      treasure.setRandomPosition();
+    }
+  } else return false;
 }
 
+function checkIfWin(){
+  if(player.points === winningScore) {
+    alert("Congratulations! You win!");
+    player.row = 0; 
+    player.col = 0;      
+    player.points = 0;
+    treasure.setRandomPosition();
+    }
+}
+
+// draw grid, player and treasure
 drawGrid();
 drawPlayer();
 drawTreasure();
 
+//define onkeydown event function
 document.onkeydown = function(event) {
   ctx.clearRect(0,0,width,height);
   drawGrid();
@@ -72,17 +94,10 @@ document.onkeydown = function(event) {
       imgPlayer.src = "images/character-down.png"
       break;
   }
-  if(checkIfFoundTreasure()){
-    treasure.setRandomPosition();
-    player.points ++;
-    if(player.points === 10) {
-      alert("Congratulations! You win!");
-      player.row = 0; 
-      player.col = 0;
-      treasure.setRandomPosition();
-    }
-  };
+  checkIfFoundTreasure();
+  checkIfWin();
   drawPlayer();
   drawTreasure();
+  //update the score
   document.querySelector(".points").innerHTML = player.points;
 }
